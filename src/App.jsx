83cc6ace3e664c467b13246885d887516a27601c
israt Jahan Naishe <israt.ict.cou@gmail.com>
2024-04-18 09:26:40 +0300

    simply modified by moving response.data into notes.js

--- src/App.jsx
+++ src/App.jsx
@@ -10,23 +10,22 @@ const App = () => {
   const [showAll, setShowAll] = useState(true)
 
   const toggleImportanceOf = (id) => {
-    const url = `http://localhost:3001/notes/${id}`
     const note = notes.find(n => n.id === id)
     const changedNote = { ...note, important: !note.important }
     
-    axios.put(url, changedNote).then(response => {
-      setNotes(notes.map(note => note.id !== id ? note : response.data))
-    })
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
   }
 
 
   useEffect(() => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        console.log('promise fulfilled')
-        setNotes(response.data)
+    noteService
+    .getAll()
+    .then(initialNotes => {
+      setNotes(initialNotes)
       })
   }, [])
   console.log('render', notes.length, 'notes')
@@ -37,10 +36,10 @@ const App = () => {
       content: newNote,
       important: Math.random() > 0.5,
     }
-    axios
-      .post('http://localhost:3001/notes', noteObject)
-      .then(response => {
-        setNotes(notes.concat(noteObject))
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
         setNewNote('')
       })
   }
